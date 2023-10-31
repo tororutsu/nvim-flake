de20dd9ad76e45999ca61fccd5863aeb159381ff
tororutsu <rjahrj@gmail.com>
2023-10-31 19:00:39 -0400

    Trying to get yuck file to load

--- config/default.nix
+++ config/default.nix
@@ -1,8 +1,12 @@
-{self, ...}: {
+{self, pkgs, ...}: {
   # Import all your configuration modules here
   imports = [
     ./bufferline.nix
     ./options.nix
     ./lsp
   ];
+
+  config = {
+    extraPlugins = with pkgs.vimPlugins; [yuck-vim];
+  };
 }
